5c89c62c4effe3e59492996dd25195aa2ed2cbe6
Vishwa Kurakundi <vkurakundi@VKURAKUNDI-MBP.local>
2017-04-27 14:34:34 -0400

    Made changes to manage all the scripts in an efficient way

---
+++ Test_Scripts/setroutes.py
@@ -0,0 +1,27 @@
+# This script is to set the Routes
+
+import boto3
+
+ec2 = boto3.client('ec2', region_name='us-west-1')  # Set Region
+
+rtb = ec2.create_route_table(
+    VpcId='vpc-xxxxx'                               # Set VPC ID
+)
+
+rtbId = rtb['RouteTable']['RouteTableId']
+
+rtbroute = ec2.create_route(
+    RouteTableId=rtbId,
+    GatewayId='igw-1xxxxx',                 # Set the Internet Gateway ID
+    DestinationCidrBlock='0.0.0.0/0'        # Set Destination CIDR Block
+)
+
+rtbTags = ec2.create_tags(
+    Resources=[rtbId],
+    Tags=[
+    {
+    'Key': 'Name',
+    'Value': 'NewTestRoute'                 # Set Route Table Route
+    },
+    ]
+)
